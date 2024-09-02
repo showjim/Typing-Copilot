8827ab18a353603c15371a20bdfad73acd726e8a
chao zhou <zhouchao486@gmail.com>
2024-09-02 23:39:11 +0800

    use ollama lib

---
+++ typing-copilot.py
@@ -0,0 +1,134 @@
+import time
+from string import Template
+
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
+import pyperclip
+from datetime import date
+
+import ollama, httpx
+
+controller = Controller()
+
+
+class OllamaChatBot():
+    def __init__(self):
+        super().__init__()
+        self.OLLAMA_BASE = "http://localhost:11434"
+        self.OLLAMA_ENDPOINT = "http://localhost:11434/api/generate"
+        self.OLLAMA_CONFIG = {
+            "model": "qwen2:0.5b",
+            "keep_alive": "5m",
+            "stream": False,
+        }
+        self.client = ollama.Client(host=self.OLLAMA_BASE)
+
+        self.FIX_PROMPT_TEMPLATE = Template(
+            """Fix all typos and casing and punctuation in this text, but preserve all new line characters:
+        
+        $text
+        
+        Return only the corrected text, don't include a preamble.
+        """
+        )
+
+        self.INSTR_PROMPT_TEMPLATE = Template(
+            """You are AI assistant, a large language model trained by human, based on the AI architecture.
+        Knowledge cutoff: 2023-04
+        Current date: $date
+        The request :
+        
+        $text
+        """
+        )
+
+    def fix_text(self, text):
+        prompt = self.FIX_PROMPT_TEMPLATE.substitute(text=text)
+        if 0:
+            response = httpx.post(
+                self.OLLAMA_ENDPOINT,
+                json={"prompt": prompt, **self.OLLAMA_CONFIG},
+                headers={"Content-Type": "application/json"},
+                timeout=10,
+            )
+        else:
+            response = self.client.generate(prompt=prompt, **self.OLLAMA_CONFIG)
+        return response['response']
+
+    def instr_text(self, text):
+        cur_date = date.today()
+        prompt = self.INSTR_PROMPT_TEMPLATE.substitute(date=cur_date, text=text)
+        if 0:
+            response = httpx.post(
+                self.OLLAMA_ENDPOINT,
+                json={"prompt": prompt, **self.OLLAMA_CONFIG},
+                headers={"Content-Type": "application/json"},
+                timeout=10,
+            )
+        else:
+            response = self.client.generate(prompt=prompt, **self.OLLAMA_CONFIG)
+        return response['response']
+
+    def fix_current_line(self, usecase="fix"):
+        # macOS short cut to select current line: Cmd+Shift+Left
+        # win short cut to select current line: shift+home
+        with controller.pressed(Key.shift):
+            controller.tap(Key.home)
+        time.sleep(0.1)
+        if usecase == "fix":
+            self.fix_selection(usecase="fix")
+        elif usecase == "instruct":
+            self.fix_selection(usecase="instruct")
+
+    def fix_selection(self, usecase="fix"):
+        # 1. Copy selection to clipboard
+        with controller.pressed(Key.ctrl):  # cmd
+            controller.tap("c")
+
+        # 2. Get the clipboard string
+        time.sleep(0.1)
+        text = pyperclip.paste()
+
+        # 3. Fix string
+        if not text:
+            return
+        if usecase == "fix":
+            fixed_text = self.fix_text(text)
+        elif usecase == "instruct":
+            fixed_text = self.instr_text(text)
+        if not fixed_text:
+            return
+
+        # 4. Paste the fixed string to the clipboard
+        pyperclip.copy(fixed_text)
+        time.sleep(0.1)
+
+        # 5. Paste the clipboard and replace the selected text
+        with controller.pressed(Key.ctrl):  # .cmd
+            controller.tap("v")
+
+
+chatbot = OllamaChatBot()
+
+
+def on_f9():
+    chatbot.fix_current_line(usecase="fix")
+
+
+def on_f10():
+    chatbot.fix_current_line(usecase="instruct")
+
+
+def on_f11():
+    chatbot.fix_selection(usecase="instruct")
+
+
+def main():
+    # on Win, '<120>'==F9, '<121>'==F10
+    # on Mac, '<101>'==F9, '<109>'==F10
+    with keyboard.GlobalHotKeys({'<101>': on_f9, '<109>': on_f10, '<122>': on_f11}) as h:
+        h.join()
+
+
+if __name__ == "__main__":
+    main()
